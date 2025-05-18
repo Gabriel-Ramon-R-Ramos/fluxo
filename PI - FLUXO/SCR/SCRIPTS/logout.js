1a30d92ee105ee15fcbdd777d20244cc3fd51fc0
Gabriel Ramon <garamon97@gmail.com>
2025-05-17 22:13:44 -0300

    Corrigindo o logout para mobile

--- PI - FLUXO/SCR/SCRIPTS/logout.js
+++ PI - FLUXO/SCR/SCRIPTS/logout.js
@@ -1,14 +1,27 @@
-// === Remove o token e redireciona para a tela de login ===
-const logoutBtn = document
-  .getElementById("logout")
-  .addEventListener("click", () => {
-    // Alerta de confirmação
-    const confirmar = confirm("Deseja realmente sair?");
-    if (!confirmar) return;
+// = === Remove o token e redireciona para a tela de login === =
 
-    // Remove o token
-    localStorage.removeItem("token");
+// === Para Computador ===
+document.getElementById("logout").addEventListener("click", () => {
+  // Alerta de confirmação
+  const confirmar = confirm("Deseja realmente sair?");
+  if (!confirmar) return;
 
-    // Redireciona para a tela de login
-    window.location.href = "index.html";
-  });
+  // Remove o token
+  localStorage.removeItem("token");
+
+  // Redireciona para a tela de login
+  window.location.href = "index.html";
+});
+
+// === Para Mobile ===
+document.getElementById("logout-mobile").addEventListener("click", () => {
+  // Alerta de confirmação
+  const confirmar = confirm("Deseja realmente sair?");
+  if (!confirmar) return;
+
+  // Remove o token
+  localStorage.removeItem("token");
+
+  // Redireciona para a tela de login
+  window.location.href = "index.html";
+});
